e8c9faea1f57087697fac517a225807e2eda233c
vaqefpayfoon <31362684+vaqefpayfoon@users.noreply.github.com>
2024-09-26 19:28:14 +0330

    init

---
+++ kafka.consumer/Program.cs
@@ -0,0 +1,14 @@
+﻿using Confluent.Kafka;
+
+var consumerConfig = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "sample-group",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<string, string>(consumerConfig).Build();
+consumer.Subscribe("sample-topic");
+
+var consumeResult = consumer.Consume();
+Console.WriteLine($"Message consumed: {consumeResult.Message.Value}");
---
+++ kafka.producer/Program.cs
@@ -0,0 +1,17 @@
+﻿using Confluent.Kafka;
+
+var producerConfig = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092"
+};
+
+using var producer = new ProducerBuilder<string, string>(producerConfig).Build();
+
+var message = new Message<string, string>
+{
+    Key = "sample-key",
+    Value = "Hello from Kafka in Docker!"
+};
+
+await producer.ProduceAsync("sample-topic", message);
+Console.WriteLine("Message sent!");
